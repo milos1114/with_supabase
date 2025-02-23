7fb192bfa47dccdd364c830168f465c807956aaa
milos1114 <milos1114@hotmail.com>
2025-02-23 13:41:28 +0300

    fix

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -53,10 +53,10 @@ export const updateSession = async (request: NextRequest) => {
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
-    return NextResponse.next({
-      request: {
-        headers: request.headers,
-      },
-    });
+    // return NextResponse.next({
+    //   request: {
+    //     headers: request.headers,
+    //   },
+    // });
   }
 };
